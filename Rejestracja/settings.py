3caef521e25ddb347a222b7774efe7895081f388
Tomek <tomeksporczyk@gmail.com>
2019-07-04 18:27:59 +0200

    Available appointments v.2.0 - failed

--- Rejestracja/settings.py
+++ Rejestracja/settings.py
@@ -114,13 +114,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pl-pl'
 
+USE_TZ = True
+
 TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
